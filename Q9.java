056afc306dfb39cde84e042dd7e0c7b5c4cbbc84
Sajini Madhushani <sajinimadushani256@gmail.com>
2026-05-05 18:42:17 +0530

    Add files via upload

--- Q9.java
+++ Q9.java
@@ -1,7 +1,24 @@
+import java.util.Scanner;
+
 public class Q9 {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
 
+        System.out.print("Enter principal amount (P): ");
+        double P = scan.nextDouble();
 
+        System.out.print("Enter interest rate (R %): ");
+        double R = scan.nextDouble();
+
+        System.out.print("Enter number of years (N): ");
+        int N = scan.nextInt();
+
+        double amount = P * Math.pow((1 + (R / 100.0)), N);
+
+        double earned = amount - P;
+
+        System.out.println("Final Amount: " + amount);
+        System.out.println("Money Earned: " + earned);
 
     }
 }
